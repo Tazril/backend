72645f63e57c40c94f9f96124d1daedc2ba2d964
unknown <tazrilparvez96@gmail.com>
2021-02-13 13:39:32 +0530

    DB url updated

--- data/constants.py
+++ data/constants.py
@@ -1,7 +1,11 @@
+import os
+
+DB_URL = os.environ.get('DATABASE_URL')
+# DB_URL = "sqlite:///./test.db"
 ID = "id"
 NAME = "name"
 CAPTION = "caption"
 URL = "url"
-DB_URL = "sqlite:///./test.db"
 TABLE_NAME = "memes"
 CONNECT_ARGS = "check_same_thread"
+
